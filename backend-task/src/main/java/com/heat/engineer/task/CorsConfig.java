a7c893c46fb8f244c5d12abd4645cc66e68803cd
Pujitha Siddanathi <pujitha.s97@gmail.com>
2025-01-16 13:39:27 +0000

    Backend Code commit

---
+++ backend-task/src/main/java/com/heat/engineer/task/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.heat.engineer.task;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000") // Allow requests from Next.js app
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
---
+++ backend-task/src/test/java/com/heat_engineer/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heat_engineer.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
